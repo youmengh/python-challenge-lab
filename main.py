3da0deb0ea8fa1f3d92ef7e77d96e774dc99fdc4
arid <arid@g.com>
2023-07-10 16:34:58 -0400

    changes to input function

--- main.py
+++ main.py
@@ -1,62 +1,48 @@
 import requests
 
 def input_valid(user):
-    if (not user.isalpha() and len(user) > 0):
-        print("Invalid input (enter only alphabets)")
+    if (not user.isalpha() or not len(user) > 0):
         return False
     else:
         return True
 
-# def inputs():
-#     #enter inputs
-#     x = input('Enter the city (no abbreviations)')
-#     while (not input_valid(x)):
-#         x = input("Invalid input, Try again")
+def inputs():
+    #enter inputs
+    city = input('Enter the city (no abbreviations): ')
+    while (not input_valid(city)):
+        city = input("Invalid input, Try again: ")
 
-#     y = input('Enter the State (no abbreviations)')
-#     while (not input_valid(y) ):
-#         y = input('Enter the State (no abbreviations)')
-#     return x,y
+    state = input('Enter the State (no abbreviations): ')
+    while (not input_valid(state) ):
+        state = input("Invalid input, Try again: ")
 
-# def inputs():
-#     x = input('Enter valid City')
-#     y = input('Enter valid State/country')
-#     if (not input_valid(x) or not input_valid(y)):
-#         x=""
-#         y=""
-#         return x,y
-#     else:
-#         return x,y
+    unit = input('Enter C, F, or K to specify the unit: ')
+    while unit.upper() not in ('C', 'F', 'K'):
+        unit = input('Please select one of the three Availible options (C, F, or K): ')
+    
+    if (unit == "C"):
+        unit = "metric"
+    elif(unit == "F"):
+        unit = "imperial"
+    else:
+        unit = ""
 
-# x,y = inputs()
-
-# if (x == ""):
-#     print('Invalid input try again')
-#     inputs()
+    return city, state, unit
 
 
-# user = ""
-# while (input_valid(user) == False):
-#     user.input('Enter the city (no abbreviations)')
-# x = user
-# user = ""
-# while (input_valid(user) == False):
-#     user.input('Enter the state (no abbreviations)')
-# y = user
+city,state, unit = inputs()
 
 
-x = input('Enter the city (no abbreviations)')
-y = input('Enter the state (no abbreviations)')
 
-print('Enter C, F or K for me')
-z = input()
-unit = ""
-if (z == "C"):
-    unit = "metric"
-elif(z == "F"):
-    unit = "imperial"
-else:
-    unit = ""
+
+
+city = "columbia"
+state = "maryland"
+unit = "metric"
+
+
+
+
 
 # validate user inputs
 # user should not enter a number
@@ -68,7 +54,7 @@ data = None
 # validate api call
 try:
     response = requests.get(
-        f"http://api.openweathermap.org/data/2.5/weather?q={x},{y}&limit=1&appid=51feb6438334e6502cf871203e59a9af&units={unit}")
+        f"http://api.openweathermap.org/data/2.5/weather?q={city},{state}&limit=1&appid=51feb6438334e6502cf871203e59a9af&units={unit}")
     response.raise_for_status()
     data = response.json()
 except requests.exceptions.RequestException as e:
@@ -78,5 +64,3 @@ except ValueError:
     print("Invalid JSON")
     exit(0)
 
-temp = response.json()['main']['temp']
-print(temp)
